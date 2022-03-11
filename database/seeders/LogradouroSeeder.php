b5801e919961915218fc7769fa3f340739e3af9f
jeferson chagas silva <jeferson_chagas25@hotmail.com>
2022-03-10 22:55:08 -0300

    Feito o migrate dos primeiros migrations para autenticação do sistema, criação do processo de autenticação com o laravel breeze (starter kits) e criação dos models do sistema.

---
+++ database/seeders/LogradouroSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class LogradouroSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
